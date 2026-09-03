9c4b46d400894ae99211e0fd41af8ba9d7ef61fe
Gopal kumar <gopalkumar550550@gmail.com>
2026-09-03 15:47:29 +0530

    Update social media links in Footer component

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -28,7 +28,7 @@ export default function Footer() {
                     {/* GitHub & LinkedIn Links */}
                     <div className="footer-socials">
                         <a 
-                            href="https://github.com" 
+                            href="https://https://github.com/gopalkumar9761" 
                             target="_blank" 
                             rel="noopener noreferrer" 
                             aria-label="GitHub"
@@ -39,7 +39,7 @@ export default function Footer() {
                             </svg>
                         </a>
                         <a 
-                            href="https://linkedin.com" 
+                            href="https://https://www.linkedin.com/in/gopal-kumar-48ab20430/" 
                             target="_blank" 
                             rel="noopener noreferrer" 
                             aria-label="LinkedIn"
